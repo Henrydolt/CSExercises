bdcf9e989f05ea1de69eaf707c30f53494ff680d
Henrydolt <e0005455@u.nus.edu>
2015-08-19 23:55:12 +0800

    Henrydolt

---
+++ CSExercises/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CSExercises
+{
+    public class Ex45
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                string a = Console.ReadLine();
+                string b = Console.ReadLine();
+                bool c = Ex41.InString(a, b);
+                if (c)
+                    Console.WriteLine("1");
+                else
+                    Console.WriteLine("0");
+
+            }
+            catch
+            {
+                Console.WriteLine("Your input is wrong.");
+            }
+        }
+    }
+}
